afe0fb43af59a0edf87ab19071d815cf18a3f386
blazerbots <ohsblazerbots@gmail.com>
2024-02-17 12:57:05 -0700

    New placeholder code for autonomous

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -114,8 +114,8 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  // public Command getAutonomousCommand() {
-  // //   // An example command will be run in autonomous
-  //   return Autos.exampleAuto(m_DriveSubsystem);
-  // }
+  public Command getAutonomousCommand() {
+  //   // An example command will be run in autonomous
+    return Autos.exampleAuto(m_DriveSubsystem);
+  }
 }
