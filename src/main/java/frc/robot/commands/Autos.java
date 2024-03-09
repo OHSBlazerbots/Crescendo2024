1fdf0f3ad625c61ffce2d6baed6c0d7919f47957
Maria Nguyen <pinkwinx999@gmail.com>
2024-03-09 10:24:42 -0700

    finishing touches v1

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -4,18 +4,56 @@
 
 package frc.robot.commands;
 
-// import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.Timer;
+
+import java.time.Duration;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants.AutoConstants;
 import frc.robot.subsystems.DriveSubsystem;
 
-public final class Autos {
+public class Autos extends Command{
   /** Example static factory for an autonomous command. */
-  public static Command exampleAuto(DriveSubsystem subsystem) {
-    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+    private final DriveSubsystem m_driver;
+    private final Timer timer;
+    private final double duration;
+
+  public Autos(DriveSubsystem driver, double time) {
+    m_driver = driver;
+    duration = time;
+    addRequirements(m_driver);
+    timer = new Timer();
   }
 
-  private Autos() {
-    throw new UnsupportedOperationException("This is a utility class!");
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+      m_driver.ChassisSpeeds(0.25,0.0,0.0);
+      timer.reset();
+      timer.start();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+      m_driver.driveCommand(0,1,0);// drive straight at half
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+      // end the command if we have run for a specific amount of time
+      return timer.get() > duration;
+
   }
 }
