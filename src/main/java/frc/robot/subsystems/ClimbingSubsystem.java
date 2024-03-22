508e720616e2fb286cc1ebbd2502f0970200197a
ohsblazerbots <ohsblazerbots@gmail.com>
2024-03-21 19:24:03 -0600

    Set soft limits for climber

--- src/main/java/frc/robot/subsystems/ClimbingSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbingSubsystem.java
@@ -28,18 +28,31 @@ public class ClimbingSubsystem extends SubsystemBase {
     
 
 public ClimbingSubsystem() {
-    
 
+float rightForwardLimit = (float) SmartDashboard.getNumber("Right forward Soft Limit", 700);
+float rightReverseLimit = (float)SmartDashboard.getNumber("Right reverse Soft Limit", -700);
+float leftForwardLimit = (float) SmartDashboard.getNumber("Left forward Soft Limit", 700);
+float leftReverseLimit = (float)SmartDashboard.getNumber("Left reverse Soft Limit", -700);
 
-
+SmartDashboard.putNumber("Right forward Soft Limit", rightForwardLimit);
+SmartDashboard.putNumber("Right reverse Soft Limit", rightReverseLimit);
+SmartDashboard.putNumber("Left forward Soft Limit", leftForwardLimit);
+SmartDashboard.putNumber("Left reverse Soft Limit", leftReverseLimit);
     // m_shooterController.zeroSensors();
     // m_shooterController.setPositionZero();
     m_rightMotor.restoreFactoryDefaults();
     m_leftMotor.restoreFactoryDefaults();
     m_rightMotor.setIdleMode(IdleMode.kBrake);
     m_leftMotor.setIdleMode(IdleMode.kBrake);
-
-     // Current limiting
+    m_rightMotor.enableSoftLimit(CANSparkMax.SoftLimitDirection.kForward, false);
+   m_rightMotor.enableSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, false); 
+   m_rightMotor.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, rightForwardLimit);
+   m_rightMotor.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, rightReverseLimit);
+   m_leftMotor.enableSoftLimit(CANSparkMax.SoftLimitDirection.kForward, false);
+   m_leftMotor.enableSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, false);
+   m_leftMotor.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, leftForwardLimit);
+   m_leftMotor.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, leftReverseLimit);
+    // Current limiting
      /*int TIMEOUT_MS = 10;
      m_Controller.getMotor().configPeakCurrentLimit(20, TIMEOUT_MS); // 20 Amps
      m_shooterController.getMotor().configPeakCurrentDuration(200, TIMEOUT_MS); // 200ms
@@ -52,7 +65,7 @@ public void setClimberSpeed(double speed) {
    //  System.out.println("speed=" + speed);
    //  System.out.println("dashboard=" + SmartDashboard.getNumber("Shooter/Speed Output", 0));
     m_rightMotor.set(speed);
-    m_leftMotor.set(speed);
+   m_leftMotor.set(speed);
     writeMetricsToSmartDashboard();
  }
 
